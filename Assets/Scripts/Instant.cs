da8bb3a0446ac6d7f2d76f2ad702fd0701e6f592
DOMINGUEZ LOLA <49375012@ort>
2025-05-13 12:01:39 -0300

    Boton clonar

---
+++ Assets/Scripts/Instant.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instant : MonoBehaviour
+{
+    public GameObject cuboPreFab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    public void instanciarCubo()
+    {
+        Instantiate(cuboPreFab);
+    }
+}
